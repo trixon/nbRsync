e2d307b83b08cfe5f8a0797e93b981a08b12d641
Patrik Karlström <patrik@trixon.se>
2023-01-11 16:19:25 +0100

    Add some boot logic

---
+++ rsyncfx/src/main/java/se/trixon/rsyncfx/boot/DoArgsProcessing.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 Patrik Karlström <patrik@trixon.se>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trixon.rsyncfx.boot;
+
+import org.netbeans.api.sendopts.CommandException;
+import org.netbeans.spi.sendopts.Arg;
+import org.netbeans.spi.sendopts.ArgsProcessor;
+import org.netbeans.spi.sendopts.Description;
+import org.netbeans.spi.sendopts.Env;
+import org.openide.LifecycleManager;
+import org.openide.util.NbBundle.Messages;
+
+/**
+ *
+ * @author Patrik Karlström <patrik@trixon.se>
+ */
+public class DoArgsProcessing implements ArgsProcessor {
+
+    @Arg(shortName = 'l', longName = "list")
+    @Description(
+            shortDescription = "#DoArgsProcessing.list.desc"
+    )
+    @Messages("DoArgsProcessing.list.desc=list jobs")
+    public boolean mListOption;
+
+    @Arg(shortName = 'v', longName = "version")
+    @Description(
+            shortDescription = "#DoArgsProcessing.version.desc"
+    )
+    @Messages("DoArgsProcessing.version.desc=print the version information and exit")
+    public boolean mVersionOption;
+
+    @Arg(longName = "start")
+    @Description(
+            displayName = "#DoArgsProcessing.start.name",
+            shortDescription = "#DoArgsProcessing.start.desc"
+    )
+    @Messages({
+        "DoArgsProcessing.start.name=--start <job>",
+        "DoArgsProcessing.start.desc=start job"
+    })
+    public String mStartOption;
+
+    {
+        DoOnStart.GUI = false;
+    }
+
+    public DoArgsProcessing() {
+    }
+
+    @Override
+    public void process(Env env) throws CommandException {
+        if (mVersionOption) {
+            displayVersion();
+        } else if (mListOption) {
+            listJobs();
+        } else if (mStartOption != null) {
+            startJob(mStartOption);
+        }
+
+        LifecycleManager.getDefault().exit();
+    }
+
+    private void displayVersion() {
+        System.out.println("VERSION");
+    }
+
+    private void listJobs() {
+        System.out.println("LIST JOBS");
+    }
+
+    private void startJob(String jobName) {
+        System.out.println("START JOB <%s>".formatted(jobName));
+    }
+}
