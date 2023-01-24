de599190631422db10478f58ebfb383fe71c0eba
Patrik Karlström <patrik@trixon.se>
2023-01-24 07:48:36 +0100

    Replace NavigationDrawer with a classic menu bar

--- rsyncfx/src/main/java/se/trixon/rsyncfx/ui/App.java
+++ rsyncfx/src/main/java/se/trixon/rsyncfx/ui/App.java
@@ -18,11 +18,15 @@ package se.trixon.rsyncfx.ui;
 import com.dlsc.gemsfx.util.StageManager;
 import com.dlsc.workbenchfx.Workbench;
 import com.dlsc.workbenchfx.model.WorkbenchModule;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
@@ -31,11 +35,15 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.controlsfx.control.StatusBar;
 import org.controlsfx.control.action.Action;
+import org.controlsfx.control.action.ActionGroup;
 import org.controlsfx.control.action.ActionUtils;
 import org.openide.LifecycleManager;
+import org.openide.util.Exceptions;
 import se.trixon.almond.nbp.core.ModuleHelper;
 import se.trixon.almond.util.CircularInt;
 import se.trixon.almond.util.Dict;
@@ -49,7 +57,6 @@ import se.trixon.almond.util.fx.dialogs.about.AboutPane;
 import se.trixon.almond.util.icons.material.MaterialIcon;
 import se.trixon.rsyncfx.Options;
 import se.trixon.rsyncfx.RsyncFx;
-import static se.trixon.rsyncfx.RsyncFx.getIconSizeToolBarInt;
 import se.trixon.rsyncfx.ui.common.BaseModule;
 import se.trixon.rsyncfx.ui.common.CustomTab;
 import se.trixon.rsyncfx.ui.editor.EditorModule;
@@ -63,16 +70,21 @@ public class App extends Application {
 
     public static final String APP_TITLE = "rsyncFX";
     private Action mAboutAction;
+    private Action mAboutRsyncAction;
+    private Action mHomeAction;
+    private Action mEditorAction;
     private WorkbenchModule mEditorModule;
     private Action mHelpAction;
     private WorkbenchModule mHistoryModule;
     private WorkbenchModule mMainModule;
     private final Options mOptions = Options.getInstance();
     private Action mOptionsAction;
+    private Action mQuitAction;
+    private Action mRestartAction;
     private final RsyncFx mRsyncFx = RsyncFx.getInstance();
     private Stage mStage;
-    private Workbench mWorkbench;
     private StatusBar mStatusBar = new StatusBar();
+    private Workbench mWorkbench;
 
     /**
      * @param args the command line arguments
@@ -97,10 +109,8 @@ public class App extends Application {
 
         FxHelper.runLaterDelayed(0, () -> {
             mWorkbench.openModule(mMainModule);
-            mWorkbench.openModule(mEditorModule);
-//            mWorkbench.openModule(mHistoryModule);
-            mWorkbench.openModule(mMainModule);
 //            mWorkbench.openModule(mEditorModule);
+//            mWorkbench.openModule(mHistoryModule);
 
             for (var module : mWorkbench.getModules()) {
                 if (module instanceof BaseModule baseModule) {
@@ -134,16 +144,34 @@ public class App extends Application {
                 mEditorModule,
                 mHistoryModule)
                 .tabFactory(CustomTab::new)
-                .navigationDrawerItems(
-                        ActionUtils.createMenuItem(mOptionsAction),
-                        ActionUtils.createMenuItem(mHelpAction),
-                        ActionUtils.createMenuItem(mAboutAction)
-                )
                 .build();
         mWorkbench.getStylesheets().add(App.class.getResource("baseTheme.css").toExternalForm());
         mRsyncFx.setWorkbench(mWorkbench);
 
         var root = new BorderPane(mWorkbench);
+
+        var actions = List.of(
+                new ActionGroup(Dict.FILE_MENU.toString(),
+                        mRestartAction,
+                        mQuitAction
+                ),
+                new ActionGroup(Dict.TOOLS.toString(),
+                        mHomeAction,
+                        mEditorAction,
+                        ActionUtils.ACTION_SEPARATOR,
+                        mOptionsAction
+                ),
+                new ActionGroup(Dict.HELP.toString(),
+                        mHelpAction,
+                        ActionUtils.ACTION_SEPARATOR,
+                        mAboutRsyncAction,
+                        mAboutAction
+                )
+        );
+
+        var menubar = ActionUtils.createMenuBar(actions);
+
+        root.setTop(menubar);
         root.setBottom(mStatusBar);
 
         var scene = new Scene(root);
@@ -155,26 +183,41 @@ public class App extends Application {
         System.out.println("HELP");
     }
 
+    private void displayRsyncInformation() {
+        new Thread(() -> {
+            var processBuilder = new ProcessBuilder(new String[]{mOptions.getRsyncPath()});
+            String result = "";
+
+            try {
+                var process = processBuilder.start();
+                result = IOUtils.toString(process.getErrorStream(), StandardCharsets.UTF_8);
+                process.waitFor();
+                var information = StringUtils.substringBefore(result, "Usage: rsync");
+                FxHelper.runLater(() -> {
+                    var alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.initOwner(RsyncFx.getInstance().getStage());
+
+                    alert.setTitle(Dict.ABOUT_S.toString().formatted("rsync"));
+                    alert.setGraphic(null);
+                    alert.setHeaderText(null);
+                    alert.setResizable(true);
+
+                    alert.setContentText(information);
+                    var dialogPane = alert.getDialogPane();
+
+                    dialogPane.setPrefWidth(FxHelper.getUIScaled(600));
+                    FxHelper.removeSceneInitFlicker(dialogPane);
+
+                    FxHelper.showAndWait(alert, RsyncFx.getInstance().getStage());
+                });
+            } catch (IOException | InterruptedException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }).start();
+    }
+
     private void initAccelerators() {
         var accelerators = mStage.getScene().getAccelerators();
-
-        for (int i = 0; i < 10; i++) {
-            final int index = i;
-            var r = (Runnable) () -> {
-                if (index == 0) {
-                    if (mWorkbench.getNavigationDrawer().isVisible()) {
-                        mWorkbench.hideNavigationDrawer();
-                    } else {
-                        mWorkbench.showNavigationDrawer();
-                    }
-                } else if (mWorkbench.getOpenModules().size() >= index) {
-                    mWorkbench.openModule(mWorkbench.getOpenModules().get(index - 1));
-                }
-            };
-
-            accelerators.put(new KeyCodeCombination(KeyCode.valueOf("DIGIT" + i), KeyCombination.SHORTCUT_DOWN), r);
-            accelerators.put(new KeyCodeCombination(KeyCode.valueOf("NUMPAD" + i), KeyCombination.SHORTCUT_DOWN), r);
-        }
 
         accelerators.put(new KeyCodeCombination(KeyCode.TAB, KeyCombination.SHORTCUT_DOWN), () -> {
             var openModules = mWorkbench.getOpenModules();
@@ -200,14 +243,20 @@ public class App extends Application {
             }
         });
 
+        accelerators.put(new KeyCodeCombination(KeyCode.H, KeyCombination.SHORTCUT_DOWN), () -> {
+            mHomeAction.handle(null);
+        });
+
+        accelerators.put(new KeyCodeCombination(KeyCode.E, KeyCombination.SHORTCUT_DOWN), () -> {
+            mEditorAction.handle(null);
+        });
+
         accelerators.put(new KeyCodeCombination(KeyCode.Q, KeyCombination.SHORTCUT_DOWN), () -> {
-            mStage.fireEvent(new WindowEvent(mStage, WindowEvent.WINDOW_CLOSE_REQUEST));
+            mQuitAction.handle(null);
         });
 
         accelerators.put(new KeyCodeCombination(KeyCode.Q, KeyCombination.SHORTCUT_DOWN, KeyCombination.SHIFT_DOWN), () -> {
-            var lifecycleManager = LifecycleManager.getDefault();
-            lifecycleManager.markForRestart();
-            lifecycleManager.exit();
+            mRestartAction.handle(null);
         });
 
         accelerators.put(new KeyCodeCombination(KeyCode.F1), () -> {
@@ -223,17 +272,42 @@ public class App extends Application {
     }
 
     private void initActions() {
+        //restart
+        mRestartAction = new Action(Dict.RESTART.toString(), actionEvent -> {
+            var lifecycleManager = LifecycleManager.getDefault();
+            lifecycleManager.markForRestart();
+            lifecycleManager.exit();
+        });
+        mRestartAction.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN, KeyCodeCombination.SHIFT_DOWN));
+
+        //quit
+        mQuitAction = new Action(Dict.QUIT.toString(), actionEvent -> {
+            mStage.fireEvent(new WindowEvent(mStage, WindowEvent.WINDOW_CLOSE_REQUEST));
+        });
+        mQuitAction.setAccelerator(new KeyCodeCombination(KeyCode.Q, KeyCombination.SHORTCUT_DOWN));
+
+        //home
+        mHomeAction = new Action(Dict.HOME.toString(), actionEvent -> {
+            mWorkbench.openModule(mMainModule);
+        });
+        mHomeAction.setAccelerator(new KeyCodeCombination(KeyCode.H, KeyCombination.SHORTCUT_DOWN));
+
+        //editor
+        mEditorAction = new Action(Dict.EDITOR.toString(), actionEvent -> {
+            mWorkbench.openModule(mEditorModule);
+        });
+        mEditorAction.setAccelerator(new KeyCodeCombination(KeyCode.E, KeyCombination.SHORTCUT_DOWN));
+
         //options
         mOptionsAction = new Action(Dict.OPTIONS.toString(), actionEvent -> {
-            mWorkbench.hideNavigationDrawer();
             OptionsPane.displayOptions();
         });
 
         //help
         mHelpAction = new Action(Dict.HELP.toString(), actionEvent -> {
-            mWorkbench.hideNavigationDrawer();
             displayHelp();
         });
+        mHelpAction.setAccelerator(new KeyCodeCombination(KeyCode.F1));
 
         //about
         var pomInfo = new PomInfo(App.class, "se.trixon.rsyncfx", "rsyncfx");
@@ -243,8 +317,11 @@ public class App extends Application {
 
         var aboutAction = AboutPane.getAction(mStage, aboutModel);
         mAboutAction = new Action(Dict.ABOUT_S.toString().formatted(APP_TITLE), actionEvent -> {
-            mWorkbench.hideNavigationDrawer();
             aboutAction.handle(actionEvent);
+        });
+
+        mAboutRsyncAction = new Action(Dict.ABOUT_S.toString().formatted("rsync"), actionEvent -> {
+            displayRsyncInformation();
         });
     }
 
@@ -256,10 +333,6 @@ public class App extends Application {
 
     private void updateNightMode() {
         MaterialIcon.setDefaultColor(mOptions.isNightMode() ? Color.LIGHTGRAY : Color.BLACK);
-
-        mOptionsAction.setGraphic(MaterialIcon._Action.SETTINGS.getImageView(getIconSizeToolBarInt()));
-        mHelpAction.setGraphic(MaterialIcon._Action.HELP_OUTLINE.getImageView(getIconSizeToolBarInt()));
-        mAboutAction.setGraphic(MaterialIcon._Action.INFO_OUTLINE.getImageView(getIconSizeToolBarInt()));
 
         String lightTheme = getClass().getResource("lightTheme.css").toExternalForm();
         String darkTheme = getClass().getResource("darkTheme.css").toExternalForm();
